916a8842994d2ba562aae024821cbdcd05bb1261
Adhi56 <adhiggagneja@gmail.com>
2025-03-11 16:54:59 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -37,3 +37,77 @@ print("Number of columns :",len(matrix[0]))
 print(matrix[0][1])
 print(matrix[1][0])
 print(matrix[0][2])
+
+#Print a 2D list in Matrix format
+for i in range(3):
+    for g in range(3):
+        print(matrix[i][g],end=" ")
+    print("\n") # makes new line
+
+rows=int(input("How many rows should there be in the matrix? \n"))
+cols=int(input("How many columns should there be in the matrix? \n"))
+
+matrix2=[]
+for a in range(rows):
+    temp=[]
+    for b in range(cols):
+        x=int(input("What should the elements inside the matrix be?"))
+        temp.append(x)
+    matrix2.append(temp)
+
+print(matrix2)
+
+for c in range(rows):
+    for d in range(cols):
+        print(matrix2[c][d],end=" ")
+    print("\n")
+
+n=int(input("What are the dimensions of the matrix? \n"))
+
+matrix3=[]
+for i in range(n):
+    tempory=[]
+    for j in range(n):
+        w=int(input("What are the elements inside the matrix?? \n"))
+        tempory.append(w)
+    matrix3.append(tempory)
+
+print(matrix3)
+
+for i in range(n):
+    for q in range(n):
+        print(matrix3[i][q],end=" ") 
+    print("\n")
+
+#printing diagonal elements
+
+for i in range(n):
+    print(matrix3[i][i])
+
+matrixA=[[1,3,2],[2,5,2]]
+matrixB=[[6,9,5],[1,7,4]]
+matrixAb=[[0,0,0],[0,0,0]]
+
+for i in range(2):
+    for f in range(3):
+        matrixAb[i][f]=matrixA[i][f]+matrixB[i][f]
+
+print(matrixAb)
+
+for i in range(2):
+    for d in range(3):
+        print(matrixAb[i][d],end=" ")
+    print("\n")
+
+matrixC=[[5,8,9],[7,9,10]]
+matrixD=[[1,4,6],[3,2,9]]
+matrixCD=[[0,0,0],[0,0,0]]
+
+for i in range(2):
+    for w in range(3):
+        matrixCD[i][w]=matrixC[i][w]-matrixD[i][w]
+
+print(matrixCD)
+
+
+
