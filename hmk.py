62e7a5fb15700a00370eb93abc3ba568298fbd5b
Adhi56 <adhiggagneja@gmail.com>
2025-03-11 16:03:15 +0000

    Add files via upload

---
+++ hmk.py
@@ -0,0 +1,10 @@
+items=[["keyboard","mouse"],["monitor","PC"]]
+
+print(items)
+print(items[0][1])
+print(items[1][0])
+print(items[1][1])
+
+print("Amount of rows :",len(items))
+
+print("Amount of columns :",len(items[0]))
